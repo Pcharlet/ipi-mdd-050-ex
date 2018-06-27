11ae0009d61a0387c24253dddf482b066b26cbd7
Pcharlet <charletpierre6@gmail.com>
2018-06-27 14:35:17 +0200

    ajout recherche par matricule

--- src/main/java/com/ipiecoles/java/mdd050/controller/EmployeController.java
+++ src/main/java/com/ipiecoles/java/mdd050/controller/EmployeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ipiecoles.java.mdd050.model.Employe;
@@ -26,13 +27,22 @@ import com.ipiecoles.java.mdd050.service.EmployeService;
 	  }
 	  
 	  @RequestMapping(value = "/{id}", produces = APPLICATION_JSON_CHARSET_UTF_8, method = RequestMethod.GET)
-	      public Employe findById(@PathVariable(value = "id") Long id){
-	          Employe employe = employeService.findById(id);
-	          if(employe == null){
-	              throw new EntityNotFoundException("L'employé d'identifiant : " + id + " n'a pas été trouvé.");
-	          }
-	          return employe;
-	      }
+	  public Employe findById(@PathVariable(value = "id") Long id){
+	     Employe employe = employeService.findById(id);
+	         if(employe == null){
+	             throw new EntityNotFoundException("L'employé d'identifiant : " + id + " n'a pas été trouvé.");
+	         }
+	         return employe;
+	  }
+	  
+	  @RequestMapping(value = "", produces = APPLICATION_JSON_CHARSET_UTF_8, method = RequestMethod.GET, params = "matricule")
+	  public Employe findAll(@RequestParam("matricule") String matricule){
+	     return employeService.findMyMatricule(matricule);
+	  }
+	  
+	  
+	  
+	  
 	  
 	}
 
